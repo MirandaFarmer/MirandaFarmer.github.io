90d6a855d7eff3af11654506fa014e4a7608ad14
M Farmer <48862642+MirandaFarmer@users.noreply.github.com>
2025-04-20 15:10:01 -0500

    add new details to Expertise component

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareJs, faRedhat, faPython } from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
+import { faCloud, faDatabase } from "@fortawesome/free-solid-svg-icons";
 
 const labelsFirst = [
     "JavaScript",
@@ -30,6 +31,15 @@ const labelsThird = [
     "Cloud"
 ];
 
+const labelsFourth = [
+    "SQL",
+    "Oracle",
+    "PowerBI",
+    "Toad Datapoint",
+    "Data Analysis",
+    "Data Visualization"
+];
+
 function Expertise() {
     return (
     <div className="container" id="expertise">
@@ -51,7 +61,7 @@ function Expertise() {
                 <div className="skill">
                     <FontAwesomeIcon icon={faRedhat} size="3x"/>
                     <FontAwesomeIcon icon={faPython} size="3x"/>
-                    <h3>Linux VM</h3>
+                    <h3>Linux VMs</h3>
                     <p>Experienced using Red Hat Enterprise Linux to manage Python automation and Oracle Database</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
@@ -62,12 +72,23 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
+                    <FontAwesomeIcon icon={faCloud} size="3x"/>
                     <h3>Microsoft Azure Environments</h3>
                     <p>Professional experience using Azure to update legacy systems</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsThird.map((label, index) => (
+                            <Chip key={index} className='chip' label={label} />
+                        ))}
+                    </div>
+                </div>
+                <div className="skill">
+                    <FontAwesomeIcon icon={faDatabase} size="3x"/>
+                    <h3>Data Analysis and Database Management</h3>
+                    <p>Data analysis and visualization using Toad Datapoint and PowerBI</p>
+                    <div className="flex-chips">
+                        <span className="chip-title">Tech stack:</span>
+                        {labelsFourth.map((label, index) => (
                             <Chip key={index} className='chip' label={label} />
                         ))}
                     </div>
